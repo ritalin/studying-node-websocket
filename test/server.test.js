568f2d86d7c91b2ee0a7fa42384243e75a36b33d
ktz <kazu.ktz.tamura@gmail.com>
2012-08-22 00:06:14 +0900

    規定のレスポンスが返されるか確認した

--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,11 @@
 //
 // mocha --reporter spec *.js
 //
-var server = require("../server/server");
+var should = require('should');
 
-var assert = require('node-assertthat');
+var server = require("../server/server");
+var http = require('http');
+
 
 describe("サーバーサイドのテスト", function() {
 	it ("サーバーインスタンスが生成されていること", function() {
@@ -13,5 +15,19 @@ describe("サーバーサイドのテスト", function() {
 		s.should.be.ok;
 		s.host.should.equal("0.0.0.0")
 		s.port.should.equal(8888);
+	});
+
+	it ("想定されるレスポンスが返されること", function(defferal) {
+		var s = server.start(8888);
+
+		http.get("http://127.0.0.1:8888", function(res) {
+			res.should.be.ok;
+			res.statusCode.should.equal(200);
+			res.headers['content-type'].should.equal('text/html');
+			res.on('data', function(chunk) {
+				chunk.toString().should.equal('<h1>さんぷるさーば</h1>');
+			})
+			defferal();
+		});
 	});
 });

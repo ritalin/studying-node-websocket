eb790d3b1240956e0e6b1057179f2a68b2944924
ktz <kazu.ktz.tamura@gmail.com>
2012-08-30 01:18:51 +0900

    ファイルから読み込んで出力

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write("<h1>さんぷるさーば</h1>");
+	var parts = url.parse(req.url, true);
+	
+	var path = server.documentRoot + parts.path;
+	if (fs.existsSync(path)) {
+		if (fs.statSync(path).isFile()) {
+			res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
+			var stream = fs.createReadStream(path);
+			stream.pipe(res);
+
+			res.end();
+
+			return;
+		}
+	}
+
+	// ファイルが無い場合
+	res.writeHead(404, {"Content-Type": "text/html; charset=utf-8"});
+	res.write('<h1>Content Not Found</h1>');
 	res.end();
 });
 
-exports.start = function(port) {
+exports.start = function(port, documentRoot) {
 	server.listen(port);
 
 	var address = server.address();
 	server.host = address.address;
 	server.port = address.port;
+	server.documentRoot = documentRoot;
 
 	return server;
 }
--- test/server.test.js
+++ test/server.test.js
@@ -5,29 +5,52 @@ var should = require('should');
 
 var server = require("../server/server");
 var http = require('http');
-
+var fs = require('fs');
 
 describe("サーバーサイドのテスト", function() {
+	var docRoot = __dirname + "/test.fixtures";
+
+	var s = server.start(8888, docRoot);
+
 	it ("サーバーインスタンスが生成されていること", function() {
 		server.should.be.ok;
 
-		var s = server.start(8888);
+
 		s.should.be.ok;
 		s.host.should.equal("0.0.0.0")
 		s.port.should.equal(8888);
+
+		s.documentRoot.should.equal(docRoot);
+		fs.existsSync(s.documentRoot + '/hello.html').should.be.true;
 	});
 
 	it ("想定されるレスポンスが返されること", function(defferal) {
-		var s = server.start(8888);
-
-		http.get("http://127.0.0.1:8888", function(res) {
+		http.get("http://127.0.0.1:8888/hello.html", function(res) {
 			res.should.be.ok;
 			res.statusCode.should.equal(200);
-			res.headers['content-type'].should.equal('text/html');
+			res.headers['content-type'].should.equal('text/html; charset=utf-8');
 			res.on('data', function(chunk) {
-				chunk.toString().should.equal('<h1>さんぷるさーば</h1>');
+				var expected = fs.readFileSync(s.documentRoot + '/hello.html', "utf-8")
+
+				chunk.toString().should.equal(expected);
 			})
+
 			defferal();
 		});
 	});
+
+	it ("エラーレスポンスが返されること", function(defferal) {
+		http.get("http://127.0.0.1:8888/not-exists.html", function(res) {
+			res.should.be.ok;
+			res.statusCode.should.equal(404);
+			res.headers['content-type'].should.equal('text/html; charset=utf-8');
+			res.on('data', function(chunk) {
+				chunk.toString().should.equal('<h1>Content Not Found</h1>');
+			})
+
+			defferal();
+		});
+
+	})
 });
+

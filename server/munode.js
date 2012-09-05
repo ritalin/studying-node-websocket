5a5c9e19df3a5da3f2fb3b074c221ca69767915c
ktz <kazu.ktz.tamura@gmail.com>
2012-09-06 00:50:24 +0900

    指定ワードからの選択テスト

--- server/munode.js
+++ server/munode.js
@@ -11,7 +11,6 @@ exports.init = function(wordsFactory, randomFactory) {
 		var n;
 		if (typeof(fn) === "function") {
 			n = fn();
-console.log(n);
 		}
 		else {
 			n = randomNextDefault();
@@ -20,13 +19,31 @@ console.log(n);
 		return Math.floor(n * factor) % factor;
 	}
 
+	var wordsDefault = function() {
+		return ["..."];
+	};
+
+	var doTalkChoice = function(wordsFunc, randFunc) {
+		var words;
+		if (typeof(wordsFunc) === "function") {
+			words = wordsFunc();
+		}
+		else {
+			words = wordsDefault();
+		}
+
+		var n = doRanNext(randFunc, words.length);
+
+		return words[n];
+	};
+
 	var munode = {
 		talk: function(message) {
 			if ((doRanNext(this.onRandomNext, 2) == 0)) {
 				return parrot(message);
 			}
 			else {
-				return "...";
+				return doTalkChoice(wordsFactory, this.onRandomNext);
 			}
 		},
 
--- test/munode.test.js
+++ test/munode.test.js
@@ -1,13 +1,11 @@
 var should = require('should');
 
 describe("人工無のエンジンのテスト", function() {
-	describe("おうむ返しさせる", function() {
+	it("おうむ返しさせる", function() {
 		var ranSeed = 0;
 
 		var munode = require("../server/munode").init(
-			function() {
-
-			},
+			null,
 			function() {
 				return ranSeed++;
 			}
@@ -20,15 +18,20 @@ describe("人工無のエンジンのテスト", function() {
 		munode.talk("LOL").should.equal("LOL");
 	});
 
-	describe("2回に一回沈黙", function() {
+	it("2回に一回沈黙", function() {
 		var ranSeed = 0;
 
 		var munode = require("../server/munode").init(
+			null,
 			function() {
+				var n = ranSeed++ % 3;
 
-			},
-			function() {
-				return ranSeed++ / 2;
+				if (n < 2) {
+					return n / 2;
+				}
+				else {
+					return 0;
+				}
 			}
 		);
 
@@ -37,5 +40,35 @@ describe("人工無のエンジンのテスト", function() {
 		munode.talk("LOL").should.equal("...");
 		munode.talk("World").should.equal("World");
 		munode.talk("LOL").should.equal("...");
+	});
+
+	it ("2回に一回指定ワードから選択", function() {
+		var ranSeed = 0;
+
+		var wordSeed = 0;
+		var words = ["foo", "bar", "baz", "fiz", "uge"];
+		var munode = require("../server/munode").init(
+			function() {
+				return words;
+			},
+			function() {
+				var n = ranSeed++ % 3;
+
+				if (n < 2) {
+					return n / 2;
+				}
+				else {
+					return wordSeed++ % words.length / words.length;
+				}
+			}
+		);
+
+		munode.should.be.ok;
+		munode.talk("Hello").should.equal("Hello");
+		munode.talk("LOL").should.equal("foo");
+		munode.talk("World").should.equal("World");
+		munode.talk("LOL").should.equal("bar");
+		munode.talk("Wryyyy").should.equal("Wryyyy");
+		munode.talk("LOL").should.equal("baz");
 	});
 });

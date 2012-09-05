56f09e3bbfc871637675a51c608912de884c51ab
ktz <kazu.ktz.tamura@gmail.com>
2012-09-06 00:02:09 +0900

    2回に一回無言

--- server/munode.js
+++ server/munode.js
@@ -1,12 +1,35 @@
-exports.init = function() {
+exports.init = function(wordsFactory, randomFactory) {
 	var parrot = function(message) {
 		return message;
+	};
+
+	var randomNextDefault = function() {
+		return Math.Random();
+	};
+
+	var doRanNext = function(fn, factor) {
+		var n;
+		if (typeof(fn) === "function") {
+			n = fn();
+		}
+		else {
+			n = randomNextDefault();
+		}
+
+		return Math.floor(n * factor) % factor;
 	}
 
 	var munode = {
 		talk: function(message) {
-			return parrot(message);
-		}
+			if ((doRanNext(this.onRandomNext, 2) == 0)) {
+				return parrot(message);
+			}
+			else {
+				return "...";
+			}
+		},
+
+		onRandomNext: randomFactory,
 	};
 
 	return munode;
--- test/munode.test.js
+++ test/munode.test.js
@@ -2,10 +2,20 @@ var should = require('should');
 
 describe("人工無のエンジンのテスト", function() {
 	describe("おうむ返しさせる", function() {
-		var munode = require("../server/munode").init();
+		var ranSeed = 0;
+
+		var munode = require("../server/munode").init(
+			function() {
+
+			},
+			function() {
+				return ranSeed++ / 2;
+			}
+		);
 
 		munode.should.be.ok;
 		munode.talk("Hello").should.equal("Hello");
+		munode.talk("LOL").should.equal("...");
 		munode.talk("World").should.equal("World");
 	});
 });
